b962784b60878df49592cc6fb7325bf8f0d8d6db
Haneen Naaran <haneen.na.110@gmail.com>
2023-03-05 13:30:44 +0200

    mixed mnl supports predict_proba

--- mixed_mnl_xlogit.py
+++ mixed_mnl_xlogit.py
@@ -1,6 +1,9 @@
 from xlogit import MixedLogit
 from xlogit.utils import wide_to_long
 
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import GroupShuffleSplit
+
 import UserModel
 from trained_models_utils import *
 from ContextChoiceEnv import *
@@ -9,10 +12,20 @@ class Mixed_MNL():
     def __init__(self):
         self.model = MixedLogit()
         
+    def _train_test_split(self, df): # used to split the swissmetro data to train - test
+        gss = GroupShuffleSplit(n_splits=1, test_size=0.3, random_state=42)
+        split_indices = list(gss.split(df, groups=df['custom_id']))
+        train_split_indices, test_split_indices  = split_indices[0]
+
+        train_df, test_df = df.iloc[train_split_indices], df.iloc[test_split_indices]
+        self.train_df = train_df
+        self.test_df = test_df
+        return train_df, test_df
+    
     def _prepare_data(self, X, y=None):
         
         if X == 'swissmetro':
-            df_wide = pd.read_table("http://transp-or.epfl.ch/data/swissmetro.dat", sep='\t')
+            df_wide = pd.read_table("data/swissmetro.dat", sep='\t')
 
             # Keep only observations for commute and business purposes that contain known choices
             df_wide = df_wide[(df_wide['PURPOSE'].isin([1, 3]) & (df_wide['CHOICE'] != 0))]
@@ -27,7 +40,10 @@ class Mixed_MNL():
             df['TT'], df['CO'] = df['TT']/100, df['CO']/100  # Scale variables
             annual_pass = (df['GA'] == 1) & (df['alt'].isin(['TRAIN', 'SM']))
             df.loc[annual_pass, 'CO'] = 0  # Cost zero for pass holders
-            return df
+            
+            train_df, test_df = self._train_test_split(df)
+            return train_df, test_df
+            
         
         else: # generated data     
             num_slates =  X['slate_id'].nunique()
@@ -42,12 +58,13 @@ class Mixed_MNL():
             return y, alts
     
     
-    def fit(self, X, y = None, varnames = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4']):
+    def fit(self, X, y = None, varnames = ['ASC_CAR', 'ASC_TRAIN', 'CO', 'TT', 'HE']):
         data = self._prepare_data(X, y)
         if X == 'swissmetro':
-            self.model.fit(X=data[varnames], y=data['CHOICE'], varnames=varnames,
-            alts=data['alt'], ids=data['custom_id'], avail=data['AV'],
-            panels=data["ID"], randvars={'TT': 'n', 'HE': 'n'}, n_draws=500,
+            train_df, _ =  data
+            self.model.fit(X=train_df[varnames], y=train_df['CHOICE'], varnames=varnames,
+            alts=train_df['alt'], ids=train_df['custom_id'], avail=train_df['AV'],
+            panels=train_df["ID"], randvars={'TT': 'n', 'HE': 'n'}, n_draws=500,
             optim_method='L-BFGS-B')
             self.model.summary()
         else: # generated data 
@@ -66,7 +83,9 @@ class Mixed_MNL():
             self.model.summary()
             return self
         
-    def predict_proba(self, X, varnames = ['x_0', 'x_1', 'x_2', 'x_3', 'x_4']):
+    def predict_proba(self, X, varnames = ['ASC_CAR', 'ASC_TRAIN', 'CO', 'TT', 'HE']):
+        _, proba = self.model.predict(X[varnames], varnames, alts = X['alt'], ids = X['custom_id'], return_proba = True)
+        return proba
         # num_slates =  X['slate_id'].nunique()
         # num_alts =  X.shape[0]/ num_slates
         # alts = np.tile(np.arange(num_alts), num_slates)
@@ -91,11 +110,11 @@ class Mixed_MNL():
         return mean_probas
         
 if __name__ == '__main__':
-    env = TrainContextChoiceEnvironment()
+    # env = TrainContextChoiceEnvironment()
     # generate data
     # X_train, X_test, y_train, y_test = env.generate_datasets(num_features=5, num_items=20)
     model = Mixed_MNL()
     # fitted = model.fit(X_train, y_train['Rational'])
     fitted = model.fit('swissmetro', varnames=['ASC_CAR', 'ASC_TRAIN', 'CO', 'TT', 'HE'])
-    # probas = model.predict_proba(X_test)
+    probas = model.predict_proba(model.test_df)
 
