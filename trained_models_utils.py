564e95d0c55466f1bbf1f7ab1fdb1c3e88d5760e
Haneen Naaran <haneen.na.110@gmail.com>
2023-03-11 16:22:43 +0200

    fixed training utils to support swissmetro

--- trained_models_utils.py
+++ trained_models_utils.py
@@ -5,63 +5,69 @@ import numpy as np
 import pandas as pd
 
 
-def train_models_per_user(X_train, X_test, y_train, y_test, model, user_types = ['Rational','Compromise', 'Similarity', 'Attraction']):
+def train_models_per_user(X_train, X_test, y_train, y_test, model, user_types = ['Compromise', 'Similarity', 'Attraction', 'Rational'], varnames = ['x_0', 'x_1', 'x_2', 'x_3','x_4']):
 
-  results = dict((user_type,{'accuracy': 0, 'precision': 0}) for user_type in user_types)
+#   results = dict((user_type,{'accuracy': 0, 'precision': 0}) for user_type in user_types)
 
-  _X_train = X_train[['x_0', 'x_1', 'x_2', 'x_3','x_4']]
-  _X_test =  X_test[['x_0', 'x_1', 'x_2', 'x_3','x_4']]
+  _X_train = X_train[varnames]
+  _X_test =  X_test[varnames]
 
   models = {}
   for user_type in user_types:
+    # if user_type in  ['Rational', 'Attraction'] :
+    #     continue
     _y_train = y_train[user_type].to_numpy()
     _y_test = y_test[user_type].to_numpy()
 
     clf = model.fit(_X_train, _y_train)
     models[user_type] = clf
-    y_pred = clf.predict(_X_test)
+    # y_pred = clf.predict(_X_test)
     # print(f'y_pred = {y_pred}')
-    precision = precision_score(_y_test,y_pred, zero_division=0)
-    accuracy = accuracy_score(_y_test,y_pred)
-    results[user_type]['accuracy'] = accuracy
-    results[user_type]['precision'] = precision
+    # precision = precision_score(_y_test,y_pred, zero_division=0)
+    # accuracy = accuracy_score(_y_test,y_pred)
+    # results[user_type]['accuracy'] = accuracy
+    # results[user_type]['precision'] = precision
 
-  return models, results
+  return models
 
-def recommend_items_wraper(X_train, X_test, y_train, y_test, model_dict, num_items_to_recom, user_types = ['Rational', 'Compromise', 'Similarity', 'Attraction']):
+def recommend_items_wraper(X_train, X_test, y_train, y_test, model_init, num_items_to_recom, varnames =['x_0', 'x_1', 'x_2', 'x_3','x_4'],
+                           user_types = ['Rational', 'Compromise', 'Similarity', 'Attraction']):
     recommendations_features_curr_model = {}
     # non-sklearn wrraped models:
-    if model_dict['model_name'] == "pairwise_ranker":
-        models_per_user, _ = train_pairwise_ranker_models(X_train, X_test, y_train, y_test, model_dict['model_init'])
+    if type(model_init) == 'MyRanker':
+        models_per_user, _ = train_pairwise_ranker_models(X_train, X_test, y_train, y_test, model)
         for user_name in user_types:
             recommendations_features_curr_model[user_name], _ = recommend_items_ranker(models_per_user[user_name], X_test, num_items_to_recom)
     else:
-        models_per_user, _ = train_models_per_user(X_train, X_test, y_train, y_test, model_dict['model_init'])
+        models_per_user = train_models_per_user(X_train, X_test, y_train, y_test, model_init, varnames=varnames)
         for user_name in user_types:
-            recommendations_features_curr_model[user_name], _ = recommend_items(models_per_user[user_name], X_test, num_items_to_recom)
+            # if user_name in  ['Rational', 'Attraction'] :
+            #     continue
+            recommendations_features_curr_model[user_name] = recommend_items(models_per_user[user_name], X_test, varnames=varnames, num_items_to_recom = num_items_to_recom)
 
     return recommendations_features_curr_model
 
-def recommend_items(model, X_test, num_items_to_recom = 3):
+def recommend_items(model, X_test, varnames =['x_0', 'x_1', 'x_2', 'x_3','x_4'],num_items_to_recom = 3):
   slate_grouped_X_test = X_test.groupby('slate_id')
   # print(slate_grouped_X_test)
 
-  recommendations_indices  = pd.DataFrame(columns = ['slate_id','item_index'])
-  recommendations_features = pd.DataFrame(columns = ['slate_id', 'x_0', 'x_1', 'x_2', 'x_3','x_4'])
+#   recommendations_indices  = pd.DataFrame(columns = ['slate_id','item_index'])
+  cols = ['slate_id'] + varnames if 'slate_id' not in varnames else varnames
+  recommendations_features = pd.DataFrame(columns = cols)
   for slate_id, slate_items in slate_grouped_X_test:
     # print(slate_items)
-    pred_scores = model.predict_proba(slate_items[['x_0', 'x_1', 'x_2', 'x_3','x_4']])[:, 0]  #gives a score in [0, 1]
+    pred_scores = model.predict_proba(slate_items[varnames])[:, 1]  #gives a score in [0, 1]
     # print(pred_scores)
     # get the indices that would sort the vector in descending order
     order_indices = np.argsort(pred_scores)[::-1]
     # use iloc to select the rows in the desired order
-    recom_slate_items_features = slate_items.iloc[order_indices].head(num_items_to_recom)
     recom_slate_indices = order_indices[:num_items_to_recom]
-    slate_recom_indices = pd.DataFrame({'slate_id': [slate_id] * num_items_to_recom, 'item_index':recom_slate_indices})
-    recommendations_indices = pd.concat([recommendations_indices, slate_recom_indices])
+    # slate_recom_indices = pd.DataFrame({'slate_id': [slate_id] * num_items_to_recom, 'item_index':recom_slate_indices})
+    recom_slate_items_features = slate_items.iloc[order_indices[:num_items_to_recom]]
+    # recommendations_indices = pd.concat([recommendations_indices, slate_recom_indices])
     recommendations_features = pd.concat([recommendations_features, recom_slate_items_features])
     # print(recommendations))
-  return recommendations_features, recommendations_indices
+  return recommendations_features
 
 
 def recommend_items_ranker_aux(model, X):
